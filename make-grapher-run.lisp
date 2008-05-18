29a5f9f12a1bd5691027af2cabd67a9ee92dcfa6
Jean-Philippe Barrette-LaPierre <jpb@rrette.com>
2008-05-17 21:40:49 -0400

    added memoization

--- make-grapher-run.lisp
+++ make-grapher-run.lisp
@@ -1,56 +1,9 @@
 
-(defparameter *graph* (create-graph-from-file "Makefile.complete.mk"))
-(setf (cl-graph:dot-attributes *graph*) '(:rankdir "LR"
-					  :size (8 10)
-					  :rotate 90
-					  :ratio :auto))
-
-(defmethod initialize-instance :after ((vertex cl-graph:dot-vertex-mixin) &key)
-  (setf (cl-graph:dot-attributes vertex) '(:shape :plaintext)))
-
-(setf *filtered-graph* (make-projection-graph
-			*graph*
-			(lambda (v)
-			  (search "../tmp/build" (cl-graph:element v)))))
-
-
-
-(with-open-file (stream "test.dot" :direction :output :if-exists :supersede)
-  (cl-graph:graph->dot *filtered-graph* stream
-		       :vertex-labeler (lambda (vertex stream)
-					 (format stream "~(~A~)" (cl-graph:element vertex)))
-		       :edge-labeler (lambda (edge stream)
-				       (declare (ignore stream))
-				       (format stream ""))))
-
-*graph*
-
-(build-graph *targets* *pattern-edges*)
-
-
-(graphviz-export (seed-in "../tmp/build/" *targets*))
-
-(setf *stripped-graph* (filter-graph (list (seed-in "../tmp/Named_Entity")) *graph*))
-
-*stripped-graph*
-
-(let ((graph (create-graph-from-file "Makefile.complete.mk")))
-  (graphviz-export (filter-graph (list (seed-in "../tmp/build/*")) graph)))
-
-
-(let ((graph (create-graph-from-file "Makefile.complete.mk")))
-  (graphviz-export graph))
-
-
+(graphviz-export 
+ (seed-in (create-graph-from-file "Makefile.complete.mk") "../tmp/build")
+ "test.dot")
+		 
 (graphviz-export (filter-graph (list (seed-in "../tmp/Named_Entity")) *targets*))
 
 
-(documentation 'seed-in 'function)
-
-
-(setf *other-graph* (let ((graph (create-graph-from-file "test.complete.mk")))
-		      (filter-graph (list (seed-all) (seed-rebuilding-targets)) graph)))
-
-(graphviz-export *other-graph*)
-
-
+(main '("sbcl" "-T" "Makefile.complete.mk" "-o" "test.dot"))
